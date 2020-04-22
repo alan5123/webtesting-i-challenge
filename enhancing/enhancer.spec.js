3a91c7e7e9d21fa3784b166bac2d8e521aadbe87
alan5123 <alanparker5123@gmail.com>
2020-04-22 16:30:12 -0400

    first test passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require('./enhancer.js');
+const {repair} = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: "pickaxe",
+    durability: 0
+  };
+
+    describe("repair()", function () {
+      it("should accept an item object and return a new item with the durability restored to 100", function () {
+        repair(item);
+        expect(item.durability).toBe(100);
+      });
+    });
